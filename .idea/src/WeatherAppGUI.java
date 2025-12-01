59ddf911fe7c0a797b33e824d535ba0c515c292c
emil-lette <e.reichmann@edu.letteverein.berlin>
2025-12-01 11:58:31 +0100

    test

--- .idea/src/WeatherAppGUI.java
+++ .idea/src/WeatherAppGUI.java
@@ -13,39 +13,17 @@ public class WeatherAppGUI extends JFrame {
         setSize(400, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        setLayout(new GridLayout(3, 2, 10, 10));
+        setLayout(new GridLayout(1, 2));
 
-        cityField = new JTextField("Berlin");
-        tempLabel = new JLabel("Temperatur: -");
-        weatherLabel = new JLabel("Wettercode: -");
-        refreshButton = new JButton("Aktualisieren");
+        currentCondition = new JLabel(new ImageIcon "C:\\Users\\e.reichmann\\IdeaProjects\\letteWetter\\.idea\\src\\img");
+        currentTemp = new JLabel ("10°C");
 
-        add(new JLabel("Stadt:"));
-        add(cityField);
 
-        add(tempLabel);
-        add(weatherLabel);
 
-        add(refreshButton);
+        add(currentCondition);
+        add(currentTemp);
 
-        // Button → API + JSON laden
-        refreshButton.addActionListener(e -> {
-            try {
-                OM_HttpRequest api = new OM_HttpRequest();
-                String json = api.sendRequest();
-
-                // JSON parsen
-                WeatherData data = JsonConverter.parse(json);
-
-                // GUI updaten
-                tempLabel.setText("Temperatur: " + data.temperature + " °C");
-                weatherLabel.setText("Wettercode: " + data.weatherCode);
-
-            } catch (Exception ex) {
-                ex.printStackTrace();
-                JOptionPane.showMessageDialog(this, "Fehler: " + ex.getMessage());
-            }
-        });
+        //add(refreshButton);
 
         setVisible(true);
     }
