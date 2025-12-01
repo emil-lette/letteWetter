c30ec2cac0a91ad7bf366c0cc711a8858ba75c37
emil-lette <e.reichmann@edu.letteverein.berlin>
2025-12-01 13:34:19 +0100

    gridlayout erstellt für current Temperature und wochenübersicht Montahg > Sonntag

--- .idea/src/WeatherAppGUI.java
+++ .idea/src/WeatherAppGUI.java
@@ -5,9 +5,12 @@ public class WeatherAppGUI extends JFrame {
 
     public WeatherAppGUI() {
         setTitle("Wetter App");
-        setSize(400, 400);
+        setSize(1000, 1000);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(2, 1));
+
+        /*============================================================================*/
+
 
         // current weather
         JPanel weatherPanel = new JPanel(new GridLayout(1, 2));
@@ -21,8 +24,47 @@ public class WeatherAppGUI extends JFrame {
         weatherPanel.add(currentCondition);
         weatherPanel.add(currentTemp);
 
+
+        /*============================================================================*/
+
+        // Tagesübersicht
+        JPanel tagesPanel = new JPanel(new GridLayout(3, 4));
+        JLabel morgens = new JLabel("6 Uhr");
+        JLabel morgensT = new JLabel("10°C");
+        JLabel morgensC = new JLabel("sonnig");
+
+        JLabel mittags = new JLabel("12 Uhr");
+        JLabel mittagsT = new JLabel("10°C");
+        JLabel mittagsC = new JLabel("sonnig");
+
+        JLabel abends = new JLabel("18 Uhr");
+        JLabel abendsT = new JLabel("10°C");
+        JLabel abendsC = new JLabel("sonnig");
+
+        JLabel nachts = new JLabel("24 Uhr");
+        JLabel nachtsT = new JLabel("10°C");
+        JLabel nachtsC = new JLabel("sonnig");
+
+        tagesPanel.add(morgens);
+        tagesPanel.add(mittags);
+        tagesPanel.add(abends);
+        tagesPanel.add(nachts);
+
+        tagesPanel.add(morgensC);
+        tagesPanel.add(mittagsC);
+        tagesPanel.add(abendsC);
+        tagesPanel.add(nachtsC);
+
+        tagesPanel.add(morgensT);
+        tagesPanel.add(mittagsT);
+        tagesPanel.add(abendsT);
+        tagesPanel.add(nachtsT);
+        
+        /*============================================================================*/
+
         // Wochenübersicht
-        JPanel testPanel = new JPanel(new GridLayout(3, 7));
+        JPanel wochenPanel = new JPanel(new GridLayout(3, 7));
+        
         JLabel montag = new JLabel("Montag");
         JLabel montagT = new JLabel("10°C");
         JLabel montagC = new JLabel("sonnig");
@@ -51,37 +93,38 @@ public class WeatherAppGUI extends JFrame {
         JLabel sonntagT = new JLabel("10°C");
         JLabel sonntagC = new JLabel("sonnig");
 
-        testPanel.add(montag);
-        testPanel.add(dienstag);
-        testPanel.add(mittwoch);
-        testPanel.add(donnerstag);
-        testPanel.add(freitag);
-        testPanel.add(samstag);
-        testPanel.add(sonntag);
+        wochenPanel.add(montag);
+        wochenPanel.add(dienstag);
+        wochenPanel.add(mittwoch);
+        wochenPanel.add(donnerstag);
+        wochenPanel.add(freitag);
+        wochenPanel.add(samstag);
+        wochenPanel.add(sonntag);
 
-        testPanel.add(montagC);
-        testPanel.add(dienstagC);
-        testPanel.add(mittwochC);
-        testPanel.add(donnerstagC);
-        testPanel.add(freitagC);
-        testPanel.add(samstagC);
-        testPanel.add(sonntagC);
+        wochenPanel.add(montagC);
+        wochenPanel.add(dienstagC);
+        wochenPanel.add(mittwochC);
+        wochenPanel.add(donnerstagC);
+        wochenPanel.add(freitagC);
+        wochenPanel.add(samstagC);
+        wochenPanel.add(sonntagC);
 
-        testPanel.add(montagT);
-        testPanel.add(dienstagT);
-        testPanel.add(mittwochT);
-        testPanel.add(donnerstagT);
-        testPanel.add(freitagT);
-        testPanel.add(samstagT);
-        testPanel.add(sonntagT);
+        wochenPanel.add(montagT);
+        wochenPanel.add(dienstagT);
+        wochenPanel.add(mittwochT);
+        wochenPanel.add(donnerstagT);
+        wochenPanel.add(freitagT);
+        wochenPanel.add(samstagT);
+        wochenPanel.add(sonntagT);
 
 
-
+        /*============================================================================*/
 
 
         // Panels zum JFrame hinzufügen
         add(weatherPanel);
-        add(testPanel);
+        add(wochenPanel);
+        add(tagesPanel);
 
         setVisible(true);
     }
